10ae6140db594f3ea967901bb85f35f81c207b5a
leaferjs <leaferjs@163.com>
2023-11-14 19:54:00 +0800

    update

--- packages/editor/src/editor/simulate.ts
+++ packages/editor/src/editor/simulate.ts
@@ -6,7 +6,7 @@ import { Bounds } from '@leafer-ui/core'
 
 export function simulate(editor: IEditor) {
     const { targetSimulate: simulateTarget, leafList: targetList } = editor
-    const { x, y, width, height } = new Bounds().setListWithHandle(targetList.list, (leaf: ILeaf) => leaf.worldBoxBounds)
+    const { x, y, width, height } = new Bounds().setListWithFn(targetList.list, (leaf: ILeaf) => leaf.worldBoxBounds)
 
     const parent = simulateTarget.parent = targetList.list[0].leafer.zoomLayer as IGroup
     const { scaleX, scaleY, e: worldX, f: worldY } = parent.__world
