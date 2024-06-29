72c2abed093a881edd16a7e44bc705c8928438ec
leaferjs <leaferjs@163.com>
2024-06-29 17:51:33 +0800

    update resize

---
+++ packages/resize/src/resize.ts
@@ -0,0 +1,101 @@
+
+import { Leaf, Path, Line, Text, Polygon, Group, Box, UI } from '@leafer-ui/draw'
+
+import { scaleResize, scaleResizeFontSize, scaleResizeGroup, scaleResizePath, scaleResizePoints } from './scaler'
+
+
+// leaf
+
+const leaf = Leaf.prototype
+
+leaf.scaleResize = function (scaleX: number, scaleY = scaleX, noResize?: boolean): void {
+    if (scaleX !== 1) {
+        //const { widthRange, heightRange } = this.__
+    }
+
+    if (scaleY !== 1) {
+
+    }
+
+    const data = this as UI
+    if (noResize || (data.editConfig && data.editConfig.editSize === 'scale')) {
+        data.scaleX *= scaleX
+        data.scaleY *= scaleY
+    } else {
+        if (scaleX < 0) data.scaleX *= -1, scaleX = -scaleX
+        if (scaleY < 0) data.scaleY *= -1, scaleY = -scaleY
+        this.__scaleResize(scaleX, scaleY)
+    }
+}
+
+
+leaf.__scaleResize = function (scaleX: number, scaleY: number): void {
+    scaleResize(this, scaleX, scaleY)
+}
+
+
+leaf.resizeWidth = function (width: number): void {
+    const scale = width / this.getBounds('box', 'local').width
+    this.scaleOf(this.boxBounds, scale, this.__.lockRatio ? scale : 1, true)
+}
+
+
+leaf.resizeHeight = function (height: number): void {
+    const scale = height / this.getBounds('box', 'local').height
+    this.scaleOf(this.boxBounds, this.__.lockRatio ? scale : 1, scale, true)
+}
+
+
+// UI
+
+Text.prototype.__scaleResize = function (scaleX: number, scaleY: number): void {
+    if (this.__.__autoSize && (this.__.resizeFontSize || (this.editConfig && this.editConfig.editSize === 'font-size'))) {
+        scaleResizeFontSize(this, scaleX, scaleY)
+    } else {
+        scaleResize(this, scaleX, scaleY)
+    }
+}
+
+
+Path.prototype.__scaleResize = function (scaleX: number, scaleY: number): void {
+    scaleResizePath(this, scaleX, scaleY)
+}
+
+
+Line.prototype.__scaleResize = function (scaleX: number, scaleY: number): void {
+    if (this.pathInputed) {
+        scaleResizePath(this, scaleX, scaleY)
+    } else if (this.points) {
+        scaleResizePoints(this, scaleX, scaleY)
+    } else {
+        this.width *= scaleX
+    }
+}
+
+
+Polygon.prototype.__scaleResize = function (scaleX: number, scaleY: number): void {
+    if (this.pathInputed) {
+        scaleResizePath(this, scaleX, scaleY)
+    } else if (this.points) {
+        scaleResizePoints(this, scaleX, scaleY)
+    } else {
+        scaleResize(this, scaleX, scaleY)
+    }
+}
+
+
+// group
+
+Group.prototype.__scaleResize = function (scaleX: number, scaleY: number): void {
+    scaleResizeGroup(this, scaleX, scaleY)
+}
+
+
+Box.prototype.__scaleResize = function (scaleX: number, scaleY: number): void {
+    if (this.__.__autoSize && this.children.length) {
+        scaleResizeGroup(this, scaleX, scaleY)
+    } else {
+        scaleResize(this, scaleX, scaleY)
+        if (this.__.resizeChildren) scaleResizeGroup(this, scaleX, scaleY)
+    }
+}
