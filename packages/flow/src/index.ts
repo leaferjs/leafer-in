a0ecb6365fa57108f7a48b153f13f27032faba13
leaferjs <leaferjs@163.com>
2024-09-14 20:52:35 +0800

    add flow decorator

--- packages/flow/src/index.ts
+++ packages/flow/src/index.ts
@@ -2,20 +2,36 @@ export * from '@leafer-in/resize'
 
 export { Flow } from './Flow'
 
-import { BoundsHelper, Box, UI } from '@leafer-ui/draw'
+import { BoundsHelper, Box, UI, autoLayoutType, boundsType } from '@leafer-ui/draw'
 
 import { flowX } from './layout/flowX'
 import { flowY } from './layout/flowY'
 import { autoBoundsType } from './decorate'
 
 
+const ui = UI.prototype, box = Box.prototype
+
+// addAttr
+autoLayoutType(false)(ui, 'flow')
+boundsType(0)(ui, 'gap')
+boundsType('top-left')(ui, 'flowAlign')
+boundsType(false)(ui, 'flowWrap')
+
+boundsType('box')(ui, 'itemBox')
+boundsType(true)(ui, 'inFlow')
+
+autoBoundsType()(ui, 'autoWidth')
+autoBoundsType()(ui, 'autoHeight')
+boundsType()(ui, 'lockRatio')
+boundsType()(ui, 'autoBox')
+boundsType()(ui, 'widthRange')
+boundsType()(ui, 'heightRange')
+
+
 const { copyAndSpread } = BoundsHelper
 let doFlowX = flowX, doFlowY = flowY
 
-UI.changeAttr('autoWidth', undefined, autoBoundsType)
-UI.changeAttr('autoHeight', undefined, autoBoundsType)
-
-Box.prototype.__updateFlowLayout = function (): void {
+box.__updateFlowLayout = function (): void {
     this.leafer.created = false
     const { flow } = this.__
     switch (flow) {
@@ -36,7 +52,7 @@ Box.prototype.__updateFlowLayout = function (): void {
     this.leafer.created = true
 }
 
-Box.prototype.__updateContentBounds = function (): void {
+box.__updateContentBounds = function (): void {
     const { padding } = this.__
     const layout = this.__layout
     const same = layout.contentBounds === layout.boxBounds
