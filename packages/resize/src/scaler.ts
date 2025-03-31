1e82efde6b6dbea4f6570fe045d0fead61f08932
leaferjs <leaferjs@163.com>
2025-03-31 15:42:40 +0800

    fix(resize): 修复文本存在padding时resize异常的问题

--- packages/resize/src/scaler.ts
+++ packages/resize/src/scaler.ts
@@ -48,7 +48,8 @@ export function scaleResizeFontSize(leaf: IText, scaleX: number, scaleY: number,
 
     leaf.fontSize *= fontScale
 
-    const data = leaf.__
+    const data = leaf.__, { padding } = data
+    if (padding) leaf.padding = padding instanceof Array ? padding.map(item => item * fontScale) : padding * fontScale
     if (!data.__autoWidth) leaf.width *= fontScale
     if (!data.__autoHeight) leaf.height *= fontScale
 
