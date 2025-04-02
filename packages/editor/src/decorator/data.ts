dc692bdba946d54a5d7e2395f49a8451b2592f7b
leaferjs <leaferjs@163.com>
2025-04-02 21:48:39 +0800

    fix(editor): 锁定元素不能再次被选中的问题

--- packages/editor/src/decorator/data.ts
+++ packages/editor/src/decorator/data.ts
@@ -5,17 +5,33 @@ import { defineKey } from '@leafer-ui/draw'
 import { EditorEvent } from '../event/EditorEvent'
 
 
-
 export function targetAttr(fn: IFunction) {
     return (target: ILeaf, key: string) => {
         const privateKey = '_' + key
         defineKey(target, key, {
             get() { return (this as IObject)[privateKey] },
-            set(value: unknown) {
+            set(value: IUI | IUI[]) {
                 const old = (this as IObject)[privateKey]
                 if (old !== value) {
-                    const type = key === 'target' ? EditorEvent.BEFORE_SELECT : EditorEvent.BEFORE_HOVER
-                    if (this.hasEvent(type)) this.emitEvent(new EditorEvent(type, { editor: this as IEditor, value: value as IUI, oldValue: old }));
+
+                    if ((this as IEditor).config) { // Editor
+
+                        const isSelect = key === 'target'
+                        if (isSelect) {
+                            if (value instanceof Array && value.length > 1 && value[0].locked) value.splice(0, 1) // fix: 单个锁定 + shift多选
+
+                            const { beforeSelect } = (this as IEditor).config
+                            if (beforeSelect) {
+                                const check = beforeSelect({ target: value })
+                                if (typeof check === 'object') value = check
+                                else if (check === false) return
+                            }
+                        }
+
+                        const type = isSelect ? EditorEvent.BEFORE_SELECT : EditorEvent.BEFORE_HOVER
+                        if (this.hasEvent(type)) this.emitEvent(new EditorEvent(type, { editor: this as IEditor, value: value as IUI, oldValue: old }))
+                    }
+
                     (this as IObject)[privateKey] = value, fn(this, old)
                 }
             }
--- packages/editor/src/editor/target.ts
+++ packages/editor/src/editor/target.ts
@@ -1,6 +1,6 @@
 import { LeafList } from '@leafer-ui/draw'
 
-import { IEditor, ILeaf, IUI } from '@leafer-in/interface'
+import { IEditor, IUI } from '@leafer-in/interface'
 
 import { simulate } from './simulate'
 import { updateMoveCursor } from './cursor'
@@ -10,11 +10,7 @@ import { EditorEvent } from '../event/EditorEvent'
 export function onTarget(editor: IEditor, oldValue: IUI | IUI[]): void {
     const { target } = editor
     if (target) {
-        const { list } = editor.leafList = target instanceof LeafList ? target : new LeafList(target instanceof Array ? target : target as IUI)
-        if (!list.every(checkEditable)) { // 过滤不合格的元素
-            editor.target = list.filter(checkEditable) as IUI[]
-            return
-        }
+        editor.leafList = target instanceof LeafList ? target : new LeafList(target)
         if (editor.multiple) simulate(editor) // 更新模拟元素
     } else {
         editor.simulateTarget.remove()
@@ -41,9 +37,4 @@ export function onTarget(editor: IEditor, oldValue: IUI | IUI[]): void {
 
 export function onHover(editor: IEditor, oldValue: IUI): void {
     editor.emitEvent(new EditorEvent(EditorEvent.HOVER, { editor, value: editor.hoverTarget, oldValue }))
-}
-
-
-function checkEditable(item: ILeaf): boolean {
-    return item.editable && !item.locked
 }
