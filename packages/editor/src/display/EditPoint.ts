c776718d5909d863b74bb9ec99bcbae10bbbf4fb
leaferjs <leaferjs@163.com>
2025-05-26 08:24:32 +0800

    feat(editor): 优化控制点的阴影性能

--- packages/editor/src/display/EditPoint.ts
+++ packages/editor/src/display/EditPoint.ts
@@ -1,9 +1,16 @@
 import { Box, Direction9 } from '@leafer-ui/draw'
 
-import { IEditPoint, IEditPointType } from '@leafer-in/interface'
+import { IEditPoint, IEditPointInputData, IEditPointType } from '@leafer-in/interface'
 
 
 export class EditPoint extends Box implements IEditPoint {
+
     public direction: Direction9
     public pointType: IEditPointType
+
+    constructor(data: IEditPointInputData) {
+        super(data)
+        this.useFastShadow = true
+    }
+
 }
