b848f1852544730521debb1b45871ab966b15cf2
leaferjs <leaferjs@163.com>
2025-05-27 18:24:18 +0800

    update(textEditor)

--- packages/text-editor/src/index.ts
+++ packages/text-editor/src/index.ts
@@ -1,8 +1,6 @@
 export { TextEditor } from './TextEditor'
 
-import { Plugin, Text, surfaceType } from '@leafer-ui/core'
+import { Plugin } from '@leafer-ui/core'
 
 
 Plugin.add('text-editor', 'editor')
-
-Text.addAttr('textEditing', false, surfaceType)
