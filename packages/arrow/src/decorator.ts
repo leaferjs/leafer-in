5d1e5bc9499757bcbcdfcddf5f70613bbc863a3c
leaferjs <leaferjs@163.com>
2025-11-25 19:31:22 +0800

    fix(arrow): 修复箭头反复切换有无，偶发不能更新渲染的问题

--- packages/arrow/src/decorator.ts
+++ packages/arrow/src/decorator.ts
@@ -1,13 +1,14 @@
 import { IValue, IUI } from '@leafer-ui/interface'
-import { decorateLeafAttr, attr, doStrokeType } from '@leafer-ui/draw'
+import { decorateLeafAttr, attr, doStrokeType, doBoundsType } from '@leafer-ui/draw'
 
 export function arrowType(defaultValue?: IValue) {
     return decorateLeafAttr(defaultValue, (key: string) => attr({
         set(value: IValue) {
             if (this.__setAttr(key, value)) {
                 const data = (this as IUI).__
-                data.__useArrow = data.startArrow !== 'none' || data.endArrow !== 'none'
                 doStrokeType(this)
+                const useArrow = data.startArrow !== 'none' || data.endArrow !== 'none'
+                if (useArrow || data.__useArrow !== useArrow) doBoundsType(this)
             }
         }
     }))
