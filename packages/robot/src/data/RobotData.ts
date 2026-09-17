c19f164fe67ad4d9d17da66fea283e6788484e10
leaferjs <leaferjs@163.com>
2026-09-17 16:50:58 +0800

    fix(Robot): 修复未设置fill属性在编辑器中选中不了的问题

--- packages/robot/src/data/RobotData.ts
+++ packages/robot/src/data/RobotData.ts
@@ -1,10 +1,14 @@
-import { IRobot, IRobotActionName, IRobotKeyframe } from '@leafer-ui/interface'
+import { IRobot, IRobotData, IRobotActionName, IRobotKeyframe } from '@leafer-ui/interface'
 import { UIData } from '@leafer-ui/draw'
 
 
-export class RobotData extends UIData {
+export class RobotData extends UIData implements IRobotData {
+
+    public get __isCanvas(): boolean { return true }
 
     public get __drawAfterFill(): boolean { return true }
+
+    public get __boxStroke(): boolean { return !(this as IRobotData).__pathInputed }
 
     protected _robot: IRobotKeyframe | IRobotKeyframe[]
     protected _action: IRobotActionName
